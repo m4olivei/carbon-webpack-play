c947f39b7a657118ed7d60d099e6944d48537b98
Matthew Oliveira <m4olivei@gmail.com>
2024-10-17 11:45:09 -0400

    test: demo the bundling issue wit @carbon/ibmdotcom-services-store imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import { LitElement, html } from 'lit';
-import PlayOutline from '@carbon/web-components/es/icons/play--outline/20.js';
-
-export class MyComponent extends LitElement {
-  render() {
-    return html`<p>Hello there</p>${PlayOutline()}`;
-  }
-}
-customElements.define('my-component', MyComponent);
+import '@carbon/ibmdotcom-web-components/es/components/cta/index.js';
